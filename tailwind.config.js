b6f70543aef079f3e11e99533abeef0aaaf78b3e
Amy <minds10@hotmail.com>
2021-05-04 23:19:32 -0400

    Add basic layout and styling

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or "media" or "class"
+  theme: {
+    fontFamily: {
+      "sans": ["Ubuntu", "sans-serif"],
+      "serif": ["Alegreya", "serif"],
+    },
+    zIndex: {
+      "0": 0,
+      "10": 10,
+      "-10": -10,
+      "auto": "auto",
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
